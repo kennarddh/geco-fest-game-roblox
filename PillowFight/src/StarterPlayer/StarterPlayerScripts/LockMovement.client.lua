c7ae6a7191593e60f00fc32781bda35433d1f787
kennarddh <kennarddh@gmail.com>
2022-03-23 23:50:42 +0700

    Round vector3

--- PillowFight/src/StarterPlayer/StarterPlayerScripts/LockMovement.client.lua
+++ PillowFight/src/StarterPlayer/StarterPlayerScripts/LockMovement.client.lua
@@ -21,8 +21,28 @@ local function reverseNumberPositiveNegative(num)
 	return -num
 end
 
+local function roundVector3Value(value)
+	if value == 0 then
+		return 0
+	elseif value > 0 then
+		return 1
+	else
+		return -1
+	end
+end
+
+local function roundVector3(vector3)
+	return Vector3.new(
+		0,
+		roundVector3Value(vector3.Y),
+		roundVector3Value(vector3.Z)
+	)
+end
+
 local function onUpdate()
 	local moveVectorDirection = controlModule:GetMoveVector()
+
+	moveVectorDirection = roundVector3(moveVectorDirection)
 
 	if player.Character:WaitForChild("Configuration"):WaitForChild("SpawnPad").Value == 1 then
 		moveVectorDirection = Vector3.new(
@@ -47,7 +67,6 @@ local function onUpdate()
 			end
 		end
 	end
-
 
 	player.Character.Humanoid:Move(moveVector, false)
     player.Character.Humanoid:SetStateEnabled(Enum.HumanoidStateType.Climbing, false)
